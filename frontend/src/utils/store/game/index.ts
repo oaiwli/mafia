3b4c2d65d265bcb9b92be064dfccda9b54dd1b87
genag <gena.gavrilin2007@gmail.com>
2025-06-24 23:55:52 +0300

    Added logics inc

--- frontend/src/utils/store/game/index.ts
+++ frontend/src/utils/store/game/index.ts
@@ -35,7 +35,23 @@ const useGameStore = create<Store>()((set, get) => ({
     );
   },
   inc: (key, value = 1) =>
-    set((state) => ({ [key]: (state[key] as number) + value })),
+    set((state) => {
+      const currentValue = state[key] as number;
+      const newValue = currentValue + value;
+
+      const allRoles =
+        (key === "mafiaDon" ? newValue : state.mafiaDon) +
+        (key === "mafia" ? newValue : state.mafia) +
+        (key === "killer" ? newValue : state.killer) +
+        (key === "police" ? newValue : state.police) +
+        (key === "medic" ? newValue : state.medic) +
+        (key === "whore" ? newValue : state.whore);
+
+      if (allRoles <= state.player) {
+        return { ...state, [key]: newValue };
+      }
+      return state;
+    }),
   dec: (key, value = 1) =>
     set((state) => ({ [key]: Math.max(0, (state[key] as number) - value) })),
 }));
